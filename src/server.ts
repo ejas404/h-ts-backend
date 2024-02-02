6d78b9f8490eb661868e79eb58f0cd9d7c0ebc47
ejas404 <ijasjaaass004@gmail.com>
2024-02-02 16:32:12 +0530

    config socket

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
+
 import express from 'express'
 import 'dotenv/config'
 
 
+import http from "http";
 import cors from 'cors'
 import dbConnect from './config/db.ts'
 
@@ -12,10 +14,24 @@ import { errMiddleware } from './middlewares/error_middlware.ts'
 import { studentRouter } from './routes/student_route.ts'
 import { tutorRouter } from './routes/tutor_route.ts'
 import { courseRouter } from './routes/course_route.ts'
+import {Server} from 'socket.io'
+import { BASE_URL } from './utility/constants.ts'
 
 
 const app = express()
 const PORT = process.env.PORT
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: BASE_URL
+  },
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+})  
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -32,6 +48,10 @@ app.use('/course', courseRouter)
 app.use(errMiddleware)
 
 dbConnect()
-app.listen(PORT, () => {
+ server.listen(PORT, () => {
     console.log('server started at port ' + PORT)
 })
+
+
+
+
