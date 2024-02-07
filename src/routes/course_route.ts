cd7cdffa325ac401c803adbaabcb89be12000d83
ejas404 <ijasjaaass004@gmail.com>
2024-02-07 21:35:59 +0530

    fix video bug

--- src/routes/course_route.ts
+++ src/routes/course_route.ts
@@ -6,9 +6,12 @@ import * as courseCtrl from '../controllers/course_controller.ts'
 export const courseRouter = express.Router()
 
 
-courseRouter.get('/get-course-videos/:id',videoCtrl.getCourseVidoes)
 courseRouter.get('/upcoming-courses',courseCtrl.upcomingCourses)
+
+courseRouter.get('/get-course-videos/:id',videoCtrl.getCourseVidoes)
 courseRouter.get('/get-sections/:id',videoCtrl.getSections)
+courseRouter.get('/get-video/:id',videoCtrl.getVideo)
+
 courseRouter.get('/:id',courseCtrl.getSingleCourse)
 courseRouter.get('/',courseCtrl.getCourses)
 
