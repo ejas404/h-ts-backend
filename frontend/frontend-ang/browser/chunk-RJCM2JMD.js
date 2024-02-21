61e1be5114001ff23d3706dab8446674585a45dd
ejas404 <ijasjaaass004@gmail.com>
2024-02-21 12:41:04 +0530

    add frontend

---
+++ frontend/frontend-ang/browser/chunk-RJCM2JMD.js
@@ -0,0 +1,1 @@
+import{u as a}from"./chunk-LJSVZ5L6.js";import"./chunk-4UW6SLG6.js";import"./chunk-QFHF65OI.js";export{a as UserProfileModule};
---
+++ frontend/frontend-ang/browser/chunk-XK23D3A6.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-XZMTBPZV.js";import"./chunk-IZ6O46FR.js";import"./chunk-4UW6SLG6.js";import"./chunk-QFHF65OI.js";export{a as TutorModule};
