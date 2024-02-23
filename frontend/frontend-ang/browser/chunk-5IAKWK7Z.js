918058024dba7e7d94ed826319225776fc841e59
ejas404 <ijasjaaass004@gmail.com>
2024-02-23 11:09:47 +0530

    add fronte end file with domain name

---
+++ frontend/frontend-ang/browser/chunk-5IAKWK7Z.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-6CKNFDUE.js";import"./chunk-PMAU467L.js";import"./chunk-XD47KHUP.js";import"./chunk-QFHF65OI.js";export{a as TutorModule};
---
+++ frontend/frontend-ang/browser/chunk-IA3W2VCG.js
@@ -0,0 +1,1 @@
+import{u as a}from"./chunk-ITVEWOZE.js";import"./chunk-XD47KHUP.js";import"./chunk-QFHF65OI.js";export{a as UserProfileModule};
